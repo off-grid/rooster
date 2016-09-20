03a20306bf3a3068dc5d40046fe344d334ac9cbd
owch <bowenrainyday@gmail.com>
2016-09-20 14:45:15 -0400

    Reddit comments integration

--- app/api/redditApi.js
+++ app/api/redditApi.js
@@ -5,9 +5,26 @@ module.exports = function () {
 
     var TYPE = "reddit";
 
+    var getRequestUrl = function getRequestUrl(request) {
+        var params = request.split(" ");
+
+        switch (params[0].toLowerCase()) {
+            case 'subreddit':
+                var url = "http://reddit.com/r/{subreddit}.json";
+                url = url.replace("{subreddit}",params[1]);
+                break;
+            case 'comments':
+                var url = "http://reddit.com/r/{subreddit}/comments/{commentid}.json";
+                url = url.replace("{commentid}",params[2]);
+                url = url.replace("{subreddit}",params[1]);
+                break;
+        }
+
+        return url;
+    }
+
     var getJsonByUrl = function getJsonByUrl(subreddit, callback) {
-        var url = "http://reddit.com/r/{subreddit}.json";
-        url = url.replace("{subreddit}",subreddit.trim());
+        var url = getRequestUrl(subreddit);
 
         request(url, function (error, res, body) {
             if (error) {
@@ -25,16 +42,12 @@ module.exports = function () {
         var root = {type: "", children: []};
         var parsed = JSON.parse(raw);
         var children = [];
-
         root.type = TYPE;
-
+        parsed = parsed[parsed.length - 1];
 
         for(i = 0; i < 5; i++) {
             children.push(populateNode(parsed.data.children[i], 1));
         }
-        //parsed.data.children.forEach(function(child){
-        //    children.push(populateNode(child, 1));
-        //});
 
         root.children = children;
         callback(root);
@@ -57,12 +70,28 @@ module.exports = function () {
 
         var data = raw.data;
 
-        node.text = data.title;
         node.score = data.score;
         node.author = data.author;
         node.time = data.created;
-        node.num_children = data.num_comments;
         node.id = data.id;
+
+        //Handle comments and posts differently
+        switch (raw.kind) {
+            case 't1':
+                node.text = data.body;
+                if(data.replies == ""){
+                    node.num_children = "0";
+                }
+                else{
+                    node.num_children = data.replies.data.children.length;
+                }
+
+                break ;
+            case 't3':
+                node.text = data.title;
+                node.num_children = data.num_comments;
+                break;
+        }
 
         return node;
     }
